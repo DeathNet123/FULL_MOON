ec5b6a0f217c21f880e7e3f7b30c2d7168e6225a
DeathNet123 <arslan.computer2002@gmail.com>
2020-10-29 19:23:13 +0500

    Adding new things

--- codewars/reverse.cpp
+++ codewars/reverse.cpp
@@ -5,7 +5,26 @@
 
 using namespace std;
 
+string reverse_words(string str);
+
 int main(void)
 {
+    cout<<reverse_words("This is the test.");
     return 0;
+}
+
+string reverse_words(string str)
+{
+    string cword, out;
+ for (char c : str) {
+    if (c == ' ') {
+      out += cword;
+      out += c;
+      cword = "";
+      continue;
+    }
+    cword = c+ cword;
+  }
+  out += cword;
+  return out;
 }
